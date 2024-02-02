69783533b4682f606b28fffba479e7667c0a502e
HaiquanZ <nn764562@gmail.com>
2024-02-02 23:04:56 +0700

    UI login desktop

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit{
 
+  showLogin: boolean = true;
+
   constructor(
-    
+    private commonSrv: CommonService,
+    private router: Router,
+    private fb: NonNullableFormBuilder
   ) { }
+
+  //loginForm
+  loginForm: FormGroup<{
+    email: FormControl<string>;
+    password: FormControl<string>;
+  }> = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+  //registerForm
+  registerForm: FormGroup<{
+    email: FormControl<string>;
+    password: FormControl<string>;
+    username: FormControl<string>;
+  }> = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    username: ['', [Validators.required]]
+  });
 
   ngOnInit(): void {
     
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('submit', this.loginForm.value);
+      this.commonSrv.logged.next(true);
+      this.router.navigate(['/']);
+    } else {
+      Object.values(this.loginForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  register(){
+    if (this.loginForm.valid) {
+      console.log('submit', this.loginForm.value);
+      this.commonSrv.logged.next(true);
+      this.router.navigate(['/']);
+    } else {
+      Object.values(this.loginForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  trans(){
+    this.showLogin = !this.showLogin;
   }
 
 }
