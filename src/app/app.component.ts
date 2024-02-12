4a39949867d880fe99ff2dce7f2eedb2215d5ab9
HaiquanZ <nn764562@gmail.com>
2024-02-12 22:15:37 +0700

    done sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonService } from './services/common.service';
 import { Router } from '@angular/router';
-import { NzSiderComponent } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +12,26 @@ export class AppComponent implements OnInit{
   logged: boolean = false;
   isCollapsed: boolean = false;
   textLogo: string = 'Space';
-  @ViewChild('sidebar', {static: false}) sideBar!: NzSiderComponent;
+  collapseWidth: number = 80;
+  sidebarWidth: string = '200px';
+  overlay: boolean = true;
+  titleMobile: string = 'Space';
 
-  constructor(private commonService: CommonService, private router: Router){}
+  //handle resize to solve resposive sidebar
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.handleCollapseWidth(event.target.outerWidth);
+  }
+
+  constructor(
+    private commonService: CommonService,
+    private router: Router,
+  ){
+    this.handleCollapseWidth(window.outerWidth);
+    if(window.outerWidth < 576){
+      this.isCollapsed = true;
+    }
+  }
 
 
   ngOnInit() {
@@ -31,18 +47,32 @@ export class AppComponent implements OnInit{
   }
 
   ngAfterViewInit() {
-    console.log('Sidebar: ', this.sideBar.widthSetting);
+    // setInterval(() => {
+    //   console.log('width: ', this.windowSize);
+    // }, 2000)
   }
 
   onCollapseChange(e: any){
-    if(e){
-      this.textLogo = 'S';
-    }else{
-      this.textLogo = 'Space';
-    }
+    this.textLogo = e ? 'S' : 'Space';
   }
 
   logout(){
     this.logged = false;
+  }
+
+  handleCollapseWidth(val: number){
+    if(val < 576){
+      this.collapseWidth = 0;
+      this.sidebarWidth = String(val * 0.7) + 'px';
+    }else{
+      this.collapseWidth = 80;
+      this.sidebarWidth = '200px';
+    }
+  }
+
+  handleCollapseMobile(){
+    this.isCollapsed = !this.isCollapsed;
+    this.overlay = !this.overlay;
+    this.titleMobile = this.isCollapsed ? 'Space' : '';
   }
 }
